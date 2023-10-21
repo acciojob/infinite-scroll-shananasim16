d2b14a8ee3913bf5584c10dceff83d0e78facf86
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-21 11:57:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-//your code here!
+document.addEventListener("DOMContentLoaded", function () {
+  const list = document.querySelector("#myList");
+  const itemsToAdd = 10; // Number of items to add by default
+  const itemsToAddOnScroll = 2; // Number of items to add when scrolling to the end
+
+  // Function to create a new list item
+  function createListItem(text) {
+    const li = document.createElement("li");
+    li.textContent = text;
+    return li;
+  }
+
+  // Function to add items to the list
+  function addItemsToTheList(count) {
+    for (let i = 1; i <= count; i++) {
+      const listItem = createListItem(`List Item ${list.childElementCount + i}`);
+      list.appendChild(listItem);
+    }
+  }
+
+  // Add the initial items
+  addItemsToTheList(itemsToAdd);
+
+  // Event listener to check for scrolling to the end
+  list.addEventListener("scroll", function () {
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+      addItemsToTheList(itemsToAddOnScroll);
+    }
+  });
+});
+
 
